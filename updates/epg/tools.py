556746c998124079e6d6ddef401a284a9ba3b99f
alantang <107459091+alantang1977@users.noreply.github.com>
2025-05-09 16:53:35 +0800

    Add files via upload

--- updates/epg/tools.py
+++ updates/epg/tools.py
@@ -6,9 +6,6 @@ from xml.dom import minidom
 
 
 def write_to_xml(programmes, path):
-    """
-    将节目信息写入XML文件
-    """
     root = ET.Element('tv', attrib={'date': datetime.now().strftime("%Y%m%d%H%M%S +0800")})
     for channel_id, data in programmes.items():
         channel_elem = ET.SubElement(root, 'channel', attrib={"id": channel_id})
@@ -23,9 +20,6 @@ def write_to_xml(programmes, path):
 
 
 def compress_to_gz(input_path, output_path):
-    """
-    将文件压缩为GZ格式
-    """
     with open(input_path, 'rb') as f_in:
         with gzip.open(output_path, 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
